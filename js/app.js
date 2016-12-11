f0100c2edd787ab252da412e57e26f17f6ea05a1
ishancoder <isaxena171@gmail.com>
2016-12-11 08:38:04 +0530

    changed local pic to url

--- js/app.js
+++ js/app.js
@@ -25,88 +25,88 @@ app.directive("bgImg", function() {
 app.factory("imageFactory", [function() {
     let o = {array: [
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Ishan Saxena",
             industry: "Web Development",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Rajat Patwa",
             industry: "Machine Learning",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Lalit Kishor Singh",
             industry: "Artificial Intelligence",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Abhimanyu Singh",
             industry: "Computer Science",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Gopal Singh",
             industry: "Game Development",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Naman Kumar",
             industry: "Big Data",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Omprakash",
             industry: "Java Developer",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "John Doe",
             industry: "Researcher Black Holes",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Michio Kaku",
             industry: "Astronomy",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Udit Gangwani",
             industry: "Prolanceer",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Manoj Kumar",
             industry: "Web Developer",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         },
         {
-            articleImage: "./pl.svg",
+            articleImage: "https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97300&w=400&h=300",
             author: "Atul Sharma",
             industry: "Web Developer",
             title: "Lorem Ipsum",
-            profilePic: "./user.png"
+            profilePic: "https://placeholdit.imgix.net/~text?txtsize=9&txt=100%C3%97100&w=100&h=100"
         }
     ]};
 
