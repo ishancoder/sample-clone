f5bd0a8396eedd5021fb48769c8a9bfa9306635f
ishancoder <isaxena171@gmail.com>
2016-12-11 08:32:35 +0530

    create navbar and search

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-let app = angular.module("myApp", []);
+let app = angular.module("myApp", ["ngRoute"]);
 
-app.controller("mainController", ['$scope', 'imageFactory', function($scope,imageFactory) {
+app.controller("mainController", ['$scope', 'imageFactory', '$location', function($scope, imageFactory, $location) {
     $scope.array = imageFactory.array;
+    let query = $location.hash();
+    if(query) {
+        $scope.search = query;
+    }
+
+    $scope.changeSearchParam = function(newStr) {
+        $scope.search = newStr;
+    }
 }]);
 
 app.directive("bgImg", function() {
@@ -9,7 +17,7 @@ app.directive("bgImg", function() {
         let url = attrs.bgImg;
         element.css({
             'background-image': 'url(' + url +')',
-            'background-size' : 'cover'
+            'background-position' : '50% 0'
         });
     };
 });
@@ -20,62 +28,86 @@ app.factory("imageFactory", [function() {
             articleImage: "./pl.svg",
             author: "Ishan Saxena",
             industry: "Web Development",
-            title: "Lorem Ipsum"
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
         {
             articleImage: "./pl.svg",
             author: "Rajat Patwa",
             industry: "Machine Learning",
-            title: "Lorem Ipsum"
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
         {
             articleImage: "./pl.svg",
             author: "Lalit Kishor Singh",
             industry: "Artificial Intelligence",
-            title: "Lorem Ipsum"
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
         {
             articleImage: "./pl.svg",
             author: "Abhimanyu Singh",
             industry: "Computer Science",
-            title: "Lorem Ipsum"
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
         {
             articleImage: "./pl.svg",
             author: "Gopal Singh",
             industry: "Game Development",
-            title: "Lorem Ipsum"
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
         {
             articleImage: "./pl.svg",
             author: "Naman Kumar",
             industry: "Big Data",
-            title: "Lorem Ipsum"
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
         {
             articleImage: "./pl.svg",
             author: "Omprakash",
             industry: "Java Developer",
-            title: "Lorem Ipsum"
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
         {
             articleImage: "./pl.svg",
             author: "John Doe",
             industry: "Researcher Black Holes",
-            title: "Lorem Ipsum"
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
         {
             articleImage: "./pl.svg",
             author: "Michio Kaku",
-            industry: "Astronomer",
-            title: "Lorem Ipsum"
+            industry: "Astronomy",
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
         {
             articleImage: "./pl.svg",
             author: "Udit Gangwani",
             industry: "Prolanceer",
-            title: "Lorem Ipsum"
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
         },
+        {
+            articleImage: "./pl.svg",
+            author: "Manoj Kumar",
+            industry: "Web Developer",
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
+        },
+        {
+            articleImage: "./pl.svg",
+            author: "Atul Sharma",
+            industry: "Web Developer",
+            title: "Lorem Ipsum",
+            profilePic: "./user.png"
+        }
     ]};
 
     return o;
